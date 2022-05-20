59c23ad331ccbf7e9f0537beea4bb23de8d9f750
blizzyblake278 <magicguitar19@gmail.com>
2022-05-20 17:06:05 -0500

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,14 +9,14 @@ class Quiz extends Component {
         super(props);
         this.state = { quiz_position: 1 };
     }
-
+    showNextQuestion()
     render() {
+        this.setState(quiz_position, this.state + 1)
         const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
         //sends data for the 1st question over to tthe QuizQuestion comp. 
         return <div>
             {isQuizEnd ? <QuizEnd />: 
-            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>}
-            
+            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)}/>}           
         
         
         </div>
