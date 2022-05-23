054e1fb9c1dde4f0169a0de114dd6c002c30a25a
blizzyblake278 <magicguitar19@gmail.com>
2022-05-23 09:24:20 -0500

    Completed module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,27 +2,30 @@ import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton.js';
 
 class QuizQuestion extends Component {
-    handleClick(buttonText){
-     if(buttonText == this.props.quiz_question.answer){
-         this.props.showNextQuestionHandler()
-     }
+    handleClick(buttonText) {
+        if (buttonText == this.props.quiz_question.answer) {
+            this.props.showNextQuestionHandler()
+        }
     }
     render() {
-     
-        return(
-        <main>
-            <section>
-                <p>{this.props.quiz_question.instruction_text}</p>
-            </section>
-            <section className="buttons">
-                <ul>
-                    {this.props.quiz_question.answer_options.map((answer_options, index)=>{
-                        return  <QuizQuestionButton key={index} button_text={answer_options}/>
-                    } ,onClick(this.handleClick.bind(this)))}
-                   
-                </ul>
-            </section>
-        </main >);
+
+        return (
+            <main>
+                <section>
+                    <p>{this.props.quiz_question.instruction_text}</p>
+                </section>
+                <section className="buttons">
+
+                    {/*itterates through each of the ?'s properties (answer options) by using a map that captures 
+                    the answer_option and index as local variables*/}
+                    <ul>
+                        {this.props.quiz_question.answer_options.map((answer_options, index) => {
+                            return <QuizQuestionButton key={index} button_text={answer_options} />
+                        }, onClick(this.handleClick.bind(this)))}
+
+                    </ul>
+                </section>
+            </main >);
     }
 }
 
