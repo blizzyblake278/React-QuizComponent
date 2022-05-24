57bae526be01d66931417f7dba2ff5d047cb2ad1
blizzyblake278 <magicguitar19@gmail.com>
2022-05-24 08:09:28 -0500

    Completed module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -11,7 +11,7 @@ class QuizQuestion extends Component {
 
     constructor(props){
         super(props)
-        //this.state == incorrectAnswer(this.props.incorrectAnswer(false))
+        this.state == incorrectAnswer(this.props.incorrectAnswer(false))
     }
 
     render() {
