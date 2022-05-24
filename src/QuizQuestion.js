2b2b255ea9f67eec80af2ca9001c45118851f069
blizzyblake278 <magicguitar19@gmail.com>
2022-05-24 08:08:52 -0500

    Completed module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -6,6 +6,12 @@ class QuizQuestion extends Component {
         if (buttonText == this.props.quiz_question.answer) {
             this.props.showNextQuestionHandler()
         }
+       this.setState(incorrectAnswer? false : true)
+    }
+
+    constructor(props){
+        super(props)
+        //this.state == incorrectAnswer(this.props.incorrectAnswer(false))
     }
 
     render() {
@@ -26,6 +32,7 @@ class QuizQuestion extends Component {
 
                     </ul>
                 </section>
+                {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p> : null}
             </main >);
     }
 }
